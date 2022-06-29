23e242a6a2cdea3661f47a46219311b2aec909f0
CP3-Jiratchaya-Sonsang <j.geny90@gmail.com>
2022-06-29 20:08:09 +0700

    Exercise4

---
+++ TestGit/Exercise4_Jiratchaya_S.py
@@ -0,0 +1,21 @@
+#Foundation English
+#General Business
+#Introduction to Computer Systems
+#Computer Programming
+
+print("ระบุคะแนนแต่ละวิชา")
+f=int(input("Foundation English : "))
+g=int(input("General Business : "))
+i=int(input("Introduction to Computer Systems: "))
+c=int(input("Computer Programming: "))
+
+
+print("--- Your Score ---")
+
+print("Foundation English : คะแนนที่ได้ : ",f)
+
+print("General Business : คะแนนที่ได้ : ",g)
+
+print("Introduction to Computer Systems : คะแนนที่ได้ : ",i)
+
+print("Computer Programming : คะแนนที่ได้ :",c)
